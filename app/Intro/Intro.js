b73dad96dfc993fb0097fa5512011c21c370fecc
jahde <jahde@classpass.com>
2017-07-20 12:37:18 -0400

    add everything

---
+++ app/Intro/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+const Intro = () => (
+    <View>
+        <Text>Hello from Intro</Text>
+    </View>
+);
+
+export default Intro;
---
+++ app/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+const Main = () => (
+    <View>
+        <Text>Hello from Main</Text>
+    </View>
+);
+
+export default Main;
